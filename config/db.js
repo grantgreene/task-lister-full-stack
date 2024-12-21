8179cbc2aab3057f1f14220b43c1feed6864493d
Grant Greene <grantgreene123@gmail.com>
2024-12-20 23:46:48 -0700

    Corrected text on db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connectDb() {
   try {
     const con = await mongoose.connect(process.env.MONGO_CONNECT_URI);
-    console.log(`MongoDB Connected: ${con.connection.host}`);
+    console.log(`MongoDB Connected`);
   } catch (err) {
     console.log('dberror');
   }
